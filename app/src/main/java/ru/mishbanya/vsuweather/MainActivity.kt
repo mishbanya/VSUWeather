f523d25cd90021ec4408d9c2851761a3c1856500
mishbanya <degtyarevmishas@yandex.ru>
2026-05-25 16:20:04 +0300

    feat: fragment transition

--- app/src/main/java/ru/mishbanya/vsuweather/MainActivity.kt
+++ app/src/main/java/ru/mishbanya/vsuweather/MainActivity.kt
@@ -3,6 +3,7 @@ package ru.mishbanya.vsuweather
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentTransaction.TRANSIT_FRAGMENT_FADE
 import ru.mishbanya.vsuweather.presentation.view.screens.city.CityScreenFragment
 import ru.mishbanya.vsuweather.presentation.view.screens.main.MainScreenFragment
 import ru.mishbanya.vsuweather.presentation.nav.ScreenConfig
@@ -25,17 +26,20 @@ class MainActivity : FragmentActivity() {
             is ScreenConfig.MainScreenConfig -> {
                 supportFragmentManager.popBackStack(null, androidx.fragment.app.FragmentManager.POP_BACK_STACK_INCLUSIVE)
                 supportFragmentManager.beginTransaction()
+                    .setTransition(TRANSIT_FRAGMENT_FADE)
                     .replace(R.id.fragment_container, MainScreenFragment())
                     .commit()
             }
             is ScreenConfig.CityScreenConfig -> {
                 supportFragmentManager.beginTransaction()
+                    .setTransition(TRANSIT_FRAGMENT_FADE)
                     .replace(R.id.fragment_container, CityScreenFragment.newInstance(screenConfig.cityID))
                     .addToBackStack(null)
                     .commit()
             }
             is ScreenConfig.DateScreenConfig -> {
                 supportFragmentManager.beginTransaction()
+                    .setTransition(TRANSIT_FRAGMENT_FADE)
                     .replace(R.id.fragment_container, DateScreenFragment.newInstance(screenConfig.cityID, screenConfig.date))
                     .addToBackStack(null)
                     .commit()
